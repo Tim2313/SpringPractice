9ce46c4f7bf0f1ea5ac33dc7375317d9add27231
Tim2313 <120585242+Tim2313@users.noreply.github.com>
2025-01-27 18:54:14 +0200

    [SpringPractice] Refactor code and add new files to gitignor

--- HorsesProject/src/main/java/com/example/horses/HorseApplication.java
+++ HorsesProject/src/main/java/com/example/horses/HorseApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HorseApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HorseApplication.class, args);
 	}
